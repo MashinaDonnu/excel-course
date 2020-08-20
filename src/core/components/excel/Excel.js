1e36f6386d1000f88f3dd3320be26dc10c154338
MashinaDonnu <43819117+MashinaDonnu@users.noreply.github.com>
2020-08-20 15:14:51 +0300

    Framework (#3)
    * Finish develop framework
    
    * test git functional from UI WebStorm
    
    Co-authored-by: DImka <MashinaDonnu>

---
+++ src/core/components/excel/Excel.js
@@ -0,0 +1,27 @@
+import {$} from "@core/DOM";
+
+export class Excel {
+    constructor(selector, options) {
+        this.$el = $(selector)
+        this.components = options.components || []
+    }
+    getRoot() {
+        const $root = $.create('div', 'excel')
+        const componentsArr = this.components.map( Component => {
+            const $el = $.create('div', Component.className)
+            const component = new Component($el)
+            // if (component.name) {
+            //     window['c' + component.name] = component
+            // }
+            $el.html(component.toHTML())
+            $root.append($el)
+            return component
+        })
+        this.components = componentsArr
+        return $root
+    }
+    render() {
+        this.$el.append(this.getRoot())
+        this.components.forEach(component => component.init())
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import './sccs/index.scss'
+import {Excel} from "@core/components/excel/Excel"
+import {Header} from "@core/components/header/Header";
+import {ToolBar} from "@core/components/toolbar/ToolBar";
+import {Formula} from "@core/components/formula/Formula";
+import {Table} from "@core/components/table/Table";
 
-const start = async () => {
-  return Promise.resolve()
-}
-console.log('init 1')
-
-start()
+const excel = new Excel('#app', {
+  components: [Header, ToolBar, Formula, Table]
+})
+excel.render()
