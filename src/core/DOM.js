c612af0dd034a6bcefc60a27b216f86cb1aa8894
DImka <MashinaDonnu>
2020-08-20 14:59:45 +0300

    Finish develop framework

---
+++ src/core/DOM.js
@@ -0,0 +1,46 @@
+class DOM {
+    constructor(selector) {
+        this.$el = typeof selector === 'string' ?
+            document.querySelector(selector) : selector
+    }
+    html(html) {
+        if (html) {
+            this.$el.innerHTML = html
+            return this
+        }
+        return this.$el.innerHTML.trim()
+    }
+    append(node) {
+        if (node.$el instanceof DOM) {
+            node = node.$el
+        }
+        if (Element.prototype.append) {
+            this.$el.append(node.$el)
+        } else {
+            this.$el.appendChild(node)
+        }
+    }
+    on(eventName, callback) {
+        this.$el.addEventListener(eventName, callback)
+    }
+
+    off(eventName, callback) {
+        this.$el.removeEventListener(eventName, callback)
+    }
+
+    clear() {
+        this.$el.innerHTML = ''
+    }
+}
+
+export function $(selector) {
+    return new DOM(selector)
+}
+
+$.create = (tagName, classes = '') => {
+    const el = document.createElement(tagName)
+    if (classes) {
+        el.classList.add(classes)
+    }
+    return $(el)
+}
---
+++ src/core/components/table/table.template.js
@@ -0,0 +1,46 @@
+
+const CODE = {
+    A: 65,
+    Z: 90
+}
+
+function createColl(content) {
+    return `<div class="column">${content}</div>`
+}
+
+function createCell() {
+    return `<div class="cell" contenteditable></div>`
+}
+
+
+function createRow(content, index) {
+    return `
+        <div class="row">
+            <div class="row-info">${index ? index : ''}</div>
+            <div class="row-data">${content ? content : ''}</div>
+        </div>
+    `
+}
+
+function toChar(_, index) {
+    return String.fromCharCode(CODE.A + index)
+}
+
+export function createTable(rowsCount = 15) {
+    const rows = []
+    const colsCount = CODE.Z - CODE.A + 1
+    const cols = new Array(colsCount)
+        .fill('')
+        .map(toChar)
+        .map(createColl)
+        .join('')
+    rows.push(createRow(cols))
+
+    for (let i = 0; i < rowsCount; i++) {
+        const cells = new Array(colsCount).fill(createCell()).join('')
+        rows.push(createRow(cells, i + 1))
+    }
+
+
+    return rows.join('')
+}
